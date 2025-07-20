5ae365ee80a86b27974163eda3315a50a12075ff
Rian <rian10cardosi@gmail.com>
2025-07-20 12:22:19 -0300

    projeto desenvolvido

--- conta_banco_terminal/src/main/java/br/com/ifba/ContaTerminal.java
+++ conta_banco_terminal/src/main/java/br/com/ifba/ContaTerminal.java
@@ -1,7 +1,21 @@
 package br.com.ifba;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo à GoodBank!!!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o numero da conta: ");
+        int numeroConta = sc.nextInt();
+        System.out.println("Informe o numero da agencia: ");
+        int numeroAgencia = sc.nextInt();
+        System.out.println("Informe o seu nome: ");
+        String nome = sc.next();
+        System.out.println("Informe o seu saldo: ");
+        double saldo = sc.nextDouble();
+        System.out.println("Ola, " + nome + "! Obrigado por criar uma conta em nosso banco." +
+                " Estes são os dados da sua conta: \nNumero da conta: " + numeroConta +
+                "\nNumero da agencia: " + numeroAgencia + "\nSaldo: " + saldo);
     }
 }
